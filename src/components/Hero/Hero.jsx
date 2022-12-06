2f66f8d8783c6a4e311a9ddf6f182fb2fc38a756
peterjohnedbrooke <john.edbrooke@gmail.com>
2022-12-06 14:33:31 +0000

    styling and responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,17 +9,16 @@ export default function Hero() {
                 <div className={styles.heroTextContainer}>
                     <span className={styles.spanOne}>
                         <h1>Student Life</h1>
-                        <h3>There is so much going on at Ludlow University, both on and off campus. </h3>
                     </span>
-                    <div>
-                        <span className={styles.spanTwo}>
-                            <h4>Lisa Smith</h4>
-                            <p>Student, BSc (Hons) Computer Games Design </p>
-                        </span>
+                    <span className={styles.spanTwo}><h3>There is so much going on at Ludlow University, both on and off campus. </h3></span>
+                    <span className={styles.spanThree}>
+                        <h4>Lisa Smith</h4>
+                        <p>Student, BSc (Hons) Computer Games Design </p>
+                    </span>
+                    <span className={styles.spanFour}>
                         <button><h5>View student stories</h5></button>
-                    </div>
+                    </span>
 
-                    {/* <span className={styles.spanThree}><button><h5>View student stories</h5></button></span> */}
                 </div>
                 <div className={styles.heroImgContainer}>
                     <img src={heroImg} alt="" />
